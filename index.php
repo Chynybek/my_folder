4e35d7dba248ad297db2865dc490065717108b01
Chynybek <chynybeck@gmail.com>
2021-09-11 16:42:20 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>StrtWlk</title>
+	<link rel="icon" href="img/logopng.png">
+	<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/strtwlk.css">
+
+</head>
+
+<body>
+	<form script="">
+		
+	</form>
+	<!-- header element -->
+	<header>
+		<img src="img/logo.jpg">
+		<h1>StrtWlk</h1>
+	</header>
+
+	<!-- main element -->
+	<main>
+		<h2>About StrtWlk</h2>
+		<p>Strtwlk is a streetwear shop in Kyrgyzstan</p>
+		<img src="img/scriptonite.JPG">
+		<p class="caption">Scriptonite hoodie</p>
+		
+		<!-- Advantages -->
+		<h2>Our shop's advantages</h2>
+		<ol>
+			<li>It's warm</li>
+			<li>It's stylish</li>
+			<li>It's unique</li>
+		</ol>
+		
+		<!-- Social Media -->
+		<h2>StrtWlk's social media</h2>
+		<p>Strtwlk has social media. Click on the links below to view the social medias </p>
+		<ul>
+			<li><a target="_blank" href="https://www.instagram.com/strtwlk.shop/">Instagram</a></li>
+			<li><a target="_blank" href="https://ylink.me/l/Mqve8FpP1yWy">Telegram</a></li>
+			<li><a target="_blank" href="https://ylink.me/l/mVWjPFP1Dr2O">WhatsApp</a></li>
+		</ul>
+	</main>
+	<footer>
+		<p>
+			This site was created by an IB student. &copy; Chynybek Aaliev
+		</p>
+</body>
+
+</html>
+
+
