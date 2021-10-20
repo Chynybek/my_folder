7209b5d7f1870387e93adf011b1f6fc62cc2865a
Chynybek <chynybeck@gmail.com>
2021-10-20 18:38:38 +0600

    PHP code that registers accounts into the system
    this PHP code will alert users with an error if any user is already registered with the same email address

--- register.php
+++ register.php
@@ -1,3 +1,60 @@
+<?php
+
+require_once "config_reg.php";
+require_once "session.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+
+	$fullname =trim($_POST['name']);
+	$number =trim($_POST['number']);
+	$date =trim($_POST['birth']);
+	$gender =trim($_POST['gender']);
+	$password =trim($_POST['password']);
+	$confirm_password =trim($_POST['confirm_password']);
+	$password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+	if($query = $db->prepare("SELECT * FROM clients WHERE number = ?")) {
+		$error='';
+		// Bind parameters (s = string, i =int, b=blob, etc) in our case the username is a string so we use "s"
+	$query->bind_param('s',$number);
+	$query->execute();
+	// store the result so we can check if the account exists in the database
+	$query->store_result();
+		if ($query->num_rows > 0 ){
+			$error .='<p class ="error">the number is already registered!</p>';
+		} else {
+			//Validate password
+			if(strlen($password) <5) {
+				$error .='<p class ="error">password must have at least 5 characters.</p>';
+			}
+
+			//Validate confirm password
+			if(empty($confirm_password)){
+				$error .='<p class ="error">please confirm your password.</p>';
+			} else{
+				if (empty($error) && ($password != $confirm_password)) {
+					$error .='<p class = "error>passwords did not match.</p>';
+				}
+			}
+			if(empty($error) ) {
+				$insertQuery = $db->prepare("INSERT INTO clients (name, number, password) VALUES (ClientName, MobileNumber, Password);");
+				$insertQuery->bind_param("sis",$fullname,$number,$password_hash);
+				$result =$insertQuery->execute();
+				if($result) {
+					$error .= '<p class="success">your registration was successful!</p>';
+				} else {
+					$error .= '<p class="error">something went wrong!</p>';
+				}
+			}
+		}
+	}
+	$query->close();
+	$insertQuery->close();
+	// close DB connection
+	mysqli_close($db);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -20,7 +77,7 @@ href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 						
 						<div class="form-group">
 							<label>phone number</label>
-							<input type="text" name="phone" class="form-control" required>
+							<input type="text" name="number" class="form-control" required>
 						</div>
 						
 						<div class="form-group">
